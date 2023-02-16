0791507dd47b1ac323ff999d16c0f9e486c47103
Jun Aishima <jaishima@bnl.gov>
2023-02-16 10:11:38 -0500

    add github deployment

---
+++ deploy_github_bad.py
@@ -0,0 +1,20 @@
+from prefect.orion.schemas.schedules import CronSchedule
+from prefect.deployments import Deployment
+#from blocks_alphavantage1 import get_info
+from testing import flow
+from prefect.filesystems import S3, GitHub
+
+github_block = GitHub.load("prefect-course")
+
+s3_block = S3.load("prefect-test")
+
+cron_deploy = Deployment.build_from_flow(
+    flow,
+    "flow_cron_github",
+    schedule=CronSchedule(cron="0 0 * * *", timezone="America/New_York"),
+    parameters={"name":"GOOG"},
+    storage=github_block
+)
+
+if __name__=="__main__":
+    cron_deploy.apply()
